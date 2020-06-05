8ffa9894254c9955b431f7e3fcd5563dce6a3124
Spencer Stecko <spencerstecko@gmail.com>
2020-06-04 22:33:10 -0700

    add static src files

---
+++ hgapp/static/src/js/index.js
@@ -0,0 +1,13 @@
+/* global window */
+window.jQuery = window.$ = require('jquery');
+
+const $ = window.$;
+
+require('bootstrap');
+require('eonasdan-bootstrap-datetimepicker');
+
+var moment = require('moment');
+moment().format();
+
+$(() => {
+});
