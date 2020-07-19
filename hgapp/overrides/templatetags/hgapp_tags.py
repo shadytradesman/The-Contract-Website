8854f28677f4ed8cf7c5e3834b84e9b7b3140a3d
Spencer Stecko <spencerstecko@gmail.com>
2020-07-19 07:48:43 -0700

    other error pages

--- hgapp/overrides/templatetags/hgapp_tags.py
+++ hgapp/overrides/templatetags/hgapp_tags.py
@@ -3,7 +3,14 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('tags/gwynn_art.html')
-def gwynn_art(filename):
+def gwynn_jpg(filename):
     return {
         'filename': filename,
+        'ext': ".jpg"
+    }
+@register.inclusion_tag('tags/gwynn_art.html')
+def gwynn_png(filename):
+    return {
+        'filename': filename,
+        'ext': ".png"
     }
