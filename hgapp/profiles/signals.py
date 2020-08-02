4f301d6cd6c460a42e93702174e2b05f51b980dd
Spencer Stecko <spencerstecko@gmail.com>
2020-08-02 15:30:13 -0700

    fix profile signup bug

--- hgapp/profiles/signals.py
+++ hgapp/profiles/signals.py
@@ -9,6 +9,6 @@ def make_profile_for_new_user(sender, user, **kwargs):
     new_profile = Profile(user = user,
                           id = user.id,
                           confirmed_agreements=True,
-                          date_confirmed_agreements = timezone.now)
+                          date_confirmed_agreements = timezone.now())
     new_profile.save()
     assign_perm('change_profile', user, new_profile)
