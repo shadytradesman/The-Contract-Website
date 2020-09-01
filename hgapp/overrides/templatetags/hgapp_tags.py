5a9724727f19a2e15e96a7a92767add9361ab2b4
Spencer Stecko <spencerstecko@gmail.com>
2020-09-01 08:53:50 -0700

    wiki links in account bar

--- hgapp/overrides/templatetags/hgapp_tags.py
+++ hgapp/overrides/templatetags/hgapp_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.shortcuts import get_object_or_404
+from wiki.models import Article, URLPath
 
 register = template.Library()
 
@@ -8,9 +10,26 @@ def gwynn_jpg(filename):
         'filename': filename,
         'ext': ".jpg"
     }
+
 @register.inclusion_tag('tags/gwynn_art.html')
 def gwynn_png(filename):
     return {
         'filename': filename,
         'ext': ".png"
+    }
+
+@register.inclusion_tag('tags/article_toc.html')
+def article_toc(article_path=None):
+    article = None
+    if not article_path:
+        root = URLPath.root()
+        article = root.article
+    else:
+        urlpath = URLPath.get_by_path(article_path, select_related=True)
+        article = urlpath.article
+    toc_tree = article.get_children(
+        article__current_revision__deleted=False)
+    return {
+        'article_children': toc_tree,
+        'article': article,
     }
