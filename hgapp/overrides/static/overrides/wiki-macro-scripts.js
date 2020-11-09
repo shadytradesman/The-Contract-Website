e1740620a848d2084bee775a534130850749dcfb
Spencer Stecko <spencerstecko@gmail.com>
2020-11-09 07:39:38 -0800

    guide forward/back buttons

--- hgapp/overrides/static/overrides/wiki-macro-scripts.js
+++ hgapp/overrides/static/overrides/wiki-macro-scripts.js
@@ -22,3 +22,64 @@ $(function() {
     var index= $("#js-article-index");
     $("#js-lower-index").html(index.html());
 });
+
+function findNextUrl(currentArticle, isPop) {
+    if (currentArticle.length) {
+        var child = currentArticle.find("ul>li>a");
+        if (child.length && !isPop) {
+            return child.attr("href");
+        }
+        var next = currentArticle.next();
+        if (next.length) {
+            return next.find("a").attr("href");
+        }
+        var parent = currentArticle.parent("ul").parent("li");
+        if (parent.length) {
+            return findNextUrl(parent, true);
+        } else {
+            return null;
+        }
+    } else {
+        return $(".article-toc>li>a").attr("href");
+    }
+}
+
+function findPrevUrl(currentArticle, isPop) {
+    if (currentArticle.length) {
+        var previous = currentArticle.prev();
+        if (previous.length) {
+            var child = previous.children("ul").children("li");
+            if (child.length) {
+                while( child.length ) {
+                    child = child.last().children("ul").children("li");
+                }
+                return child.end().end().parent().find("a").last().attr("href");
+            } else {
+                return previous.find("a").attr("href");
+            }
+        } else {
+            var parent = currentArticle.parent("ul").parent("li");
+            if (parent.length) {
+                return parent.find("a").attr("href");
+            }
+        }
+    }
+    return null;
+}
+
+function setNavLinks() {
+    nextUrl = findNextUrl($("#current-article"), false);
+    if (nextUrl) {
+        $(".js-next-article").attr("href", nextUrl);
+        $(".js-next-article").show();
+    }
+    previousUrl = findPrevUrl($("#current-article"), false);
+    if (previousUrl) {
+        $(".js-prev-article").attr("href", previousUrl);
+        $(".js-prev-article").show();
+    }
+}
+
+$(function() {
+    setNavLinks();
+});
