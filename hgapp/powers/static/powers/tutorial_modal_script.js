4e97ccf22f7415a0d8392abb28a3dbf3fdce03af
Spencer Stecko <spencerstecko@gmail.com>
2020-12-17 10:53:56 -0800

    misc fixes

--- hgapp/powers/static/powers/tutorial_modal_script.js
+++ hgapp/powers/static/powers/tutorial_modal_script.js
@@ -1,5 +1,30 @@
+var cookieName = "default";
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
 $(document).ready(function(){
-    if (showTutorial) {
+    var fullCookieName = "powerTutorial"+cookieName;
+    var x = getCookie(fullCookieName);
+    if (showTutorial && !x) {
         $('#tutorialModal').modal({});
+        setCookie(fullCookieName,'True',700);
     }
 });
