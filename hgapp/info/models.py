d828e6babf49ee9e352386ef1129431cbaa0db35
Spencer Stecko <spencerstecko@gmail.com>
2021-07-27 13:13:26 -0700

    info models

--- hgapp/info/models.py
+++ hgapp/info/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+from characters.models import Character, Roll
+
 class FrontPageInfo(models.Model):
     shown_fiction = models.TextField(max_length=30000)
     hidden_fiction = models.TextField(max_length=30000)
+
+class QuickStartInfo(models.Model):
+    main_char = models.ForeignKey(Character, on_delete=models.CASCADE)
+
+class ExampleAction(models.Model):
+    action = models.CharField(max_length=3000)
+    roll = models.ForeignKey(Roll, on_delete=models.CASCADE)
+    outcome_botch = models.CharField(max_length=3000)
+    outcome_partial_success = models.CharField(max_length=3000)
+    outcome_complete_success = models.CharField(max_length=3000)
+    outcome_exceptional_success = models.CharField(max_length=3000)
