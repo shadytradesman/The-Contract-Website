a978c46a671e32024adc0fd45696ec1d80b25aaa
spencer stecko <spencerstecko@spencers-MBP.T-mobile.com>
2022-03-14 21:06:25 -0700

    disable compression

--- hgapp/hgapp/settings.py
+++ hgapp/hgapp/settings.py
@@ -88,7 +88,6 @@ else:
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-    AWS_IS_GZIPPED = True
     AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
 
 
