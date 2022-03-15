a51d029e28a8d814ec8a90cefd4f540408cb9c63
spencer stecko <spencerstecko@spencers-MBP.T-mobile.com>
2022-03-14 22:37:49 -0700

    bucket-public

--- hgapp/hgapp/settings.py
+++ hgapp/hgapp/settings.py
@@ -87,6 +87,7 @@ else:
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_QUERYSTRING_AUTH = False
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
     AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
 
