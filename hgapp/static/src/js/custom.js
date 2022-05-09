aea3116c4d74dbca3837bdfd724c5266598f9b5a
spencer stecko <spencerstecko@spencers-MBP.T-mobile.com>
2022-05-09 08:46:27 -0700

    fix some display stuff

--- hgapp/static/src/js/custom.js
+++ hgapp/static/src/js/custom.js
@@ -71,27 +71,91 @@ $(function(){
 
 /* Power Keywords */
 
-const powerKeywordDict = {
+const keywordHighlights = [
     // Targeting
-	"Sapient": "An intelligent being that thinks and is self-aware.",
-	"Non-Sapient": "Anything that does not think or is not self-aware.",
-	"Living": "Beings that are alive.",
-	"Non-Living": "Anything that is not alive.",
-	"Dead": "Anything that were once alive but no longer is.",
-	"Animate": "Any being that can move or think on its own.",
-	"Construct": "Any being that is sapient and non-living.",
-	"Inanimate": "Anything that cannot move or think on its own.",
-	"Creature": "Anything that is living, animate, and non-sapient.",
-	"Object": "Anything that is non-living, inanimate, and also free-standing, loose, or otherwise not currently a part of another structure or device.",
-    "Device": "Any Object that was designed or created for some purpose.",
-    "Plant": "Any non-sapient living thing that cannot act.",
-    "Computer": "A non-living device that takes input, processes data, and produces output. Generally electric.",
-    "Vehicle": "A device designed to move from one place to another while carrying cargo or passengers.",
-    "Alien": "Something that is not of this world or not common to this world.",
+	{
+	    "regex": regexFromKeyword("Sapient"),
+	    "tooltip": "An intelligent being that thinks and is self-aware.",
+	},
+	{
+	    "regex": regexFromKeyword("Non-Sapient"),
+	    "tooltip": "Anything that does not think or is not self-aware.",
+	},
+	{
+	    "regex": regexFromKeyword("Living"),
+	    "tooltip": "Beings that are alive.",
+	},
+	{
+	    "regex": regexFromKeyword("Non-Living"),
+	    "tooltip": "Anything that is not alive.",
+	},
+	{
+	    "regex": regexFromKeyword("Dead"),
+	    "tooltip": "Anything that were once alive but no longer is.",
+	},
+	{
+	    "regex": regexFromKeyword("Animate"),
+	    "tooltip": "Any being that can move or think on its own.",
+	},
+	{
+	    "regex": regexFromKeyword("Construct"),
+	    "tooltip": "Any being that is sapient and non-living.",
+	},
+	{
+	    "regex": regexFromKeyword("Inanimate"),
+	    "tooltip": "Anything that cannot move or think on its own.",
+	},
+	{
+	    "regex": regexFromKeyword("Creature"),
+	    "tooltip": "Anything that is living, animate, and non-sapient.",
+	},
+	{
+	    "regex": regexFromKeyword("Object"),
+	    "tooltip": "Anything that is non-living, inanimate, and is not a critical component of a larger structure or device (i.e. a spark plug is not an object unless it is removed from the engine).",
+	},
+    {
+        "regex": regexFromKeyword("Device"),
+        "tooltip": "Any Object that was designed or created for some purpose.",
+    },
+    {
+        "regex": regexFromKeyword("Plant"),
+        "tooltip": "Any non-sapient living thing that cannot act.",
+    },
+    {
+        "regex": regexFromKeyword("Computer"),
+        "tooltip": "A non-living device that takes input, processes data, and produces output. Generally electric.",
+    },
+    {
+        "regex": regexFromKeyword("Vehicle"),
+        "tooltip": "A device designed to move from one place to another while carrying cargo or passengers.",
+    },
+    {
+        "regex": regexFromKeyword("Alien"),
+        "tooltip": "Something that is not of this world or is unknown to this world. For example, in a modern setting, anything that does not exist in real life, such as magic.",
+    },
+    {
+        "regex": regexFromKeyword("Non-Alien"),
+        "tooltip": "Something that is of this world or known by this world. For example, in a modern setting, anything that exists in real life.",
+    },
 
     // Other
-    "Concentration": "While concentrating you can only take Free Actions and a single Quick Action per Round. Disrupting events (like taking damage) cause the effect to end."
-};
+    {
+        "regex": regexFromKeyword("Concentration"),
+        "tooltip": "While concentrating you can only take Free Actions and a single Quick Action per Round. Disrupting events (like taking damage) cause the effect to end, and you cannot Concentrate again until the end of the next Round."
+    },
+    {
+        "regex": regexFromKeyword("Resist"),
+        "tooltip": "The target of this Effect must consent to its use or be unconscious, bound, or incapacitated."
+    },
+    {
+        "regex": regexFromKeyword("Resisted"),
+        "tooltip": "The target of this Effect must consent to its use or be unconscious, bound, or incapacitated."
+    },
+    {
+        "regex": new RegExp('[\\s](\\+[\\d]+ dice)([.,\\s])', 'gm'),
+        "tooltip": "Multiple bonuses to the same dice pool do not stack. Instead, the highest bonus is used."
+    }
+];
 
 $(document).ready(function(){
     updateHoverText();
@@ -99,16 +163,20 @@ $(document).ready(function(){
 
 function updateHoverText() {
     $('.js-render-power-keywords').each(function(){
-        for(var key in powerKeywordDict) {
-            $(this).html($(this).html().replaceAll(' ' + key, replaceSubstring));
-        }
+        $(this).html(replaceHoverText($(this).html()));
     });
     $('[data-toggle="tooltip"]').tooltip();
 }
 
-function replaceSubstring(match) {
-	const trimmed = match.trim();
-	return ' <span class="css-keyword-with-tooltip" data-toggle="tooltip" title="' + powerKeywordDict[trimmed] + '">' + trimmed + '</span>';
+function regexFromKeyword(text) {
+    return new RegExp('[\\s](' + text + "s?)([.,\\s])", 'gm');
 }
 
-
+function replaceHoverText(text) {
+    let modifiedText = text;
+    keywordHighlights.forEach(keyword => {
+        let replacementString = ' <span class="css-keyword-with-tooltip" data-toggle="tooltip" title="' + keyword.tooltip + '">$1</span>$2 '
+        modifiedText = modifiedText.replaceAll(keyword.regex, replacementString);
+    });
+    return modifiedText;
+}
