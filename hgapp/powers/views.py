abb3196b286458f0b6576fa1fcf6e660956535d4
spencer stecko <spencerstecko@spencers-MacBook-Pro.local>
2022-10-29 15:47:14 -0600

    fix memcached

--- hgapp/powers/views.py
+++ hgapp/powers/views.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import random
+import pickle
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
