f35482b80a7104973d4600bcd66269e6a64e7f63
spencer stecko <spencerstecko@spencers-MacBook-Pro.local>
2022-10-31 08:17:53 -0600

    stock gift scroll mobile

--- hgapp/powers/static/powers/stock_powers_script.js
+++ hgapp/powers/static/powers/stock_powers_script.js
@@ -30,3 +30,26 @@ $(document).ready(function(){
         setCookie(fullCookieName,'True',700);
     }
 });
+
+function isElementInViewport (el) {
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+$(".unstyled-link").on("click", function(ev){
+    setTimeout(() => {
+        let target = ev.target;
+        if (!isElementInViewport(target)) {
+            target.scrollIntoView();
+            window.scrollBy(0,-100);
+        }
+    }, 280);
+});
