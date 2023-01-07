452f726126fd2c0270f1cde4342472d756b9029d
spencer stecko <spencerstecko@spencers-MacBook-Pro.local>
2023-01-07 10:57:22 -0700

    mobile affix works

--- hgapp/overrides/static/overrides/splash_scripts.js
+++ hgapp/overrides/static/overrides/splash_scripts.js
@@ -1,21 +1,25 @@
 function setAffix() {
-    const container = $("#js-talent-section");
-    $('#js-talent-affix').affix({
-      offset: {
-        top: function () {
-        return (this.top = container.position().top - 50)
-         },
-        bottom: 0
-      }
+    $('.js-affix-image').each(function( index, value ) {
+        let container = $(value).closest(".js-affix-container");
+        $(value).affix({
+          offset: {
+            top: function () {
+                return (this.top = container.position().top - 50)
+             },
+            bottom: function () {
+                return (this.bottom = document.body.scrollHeight - container.position().top - container.outerHeight() - 50)
+            }
+          }
+        });
+        $(value).affix('checkPosition');
     });
-    $('#js-talent-affix').affix('checkPosition');
 }
 
 $(document).ready(function(){
     setAffix();
     $(window).scroll();
- setTimeout(() => {
-    setAffix();
+    setTimeout(() => {
+       setAffix();
     }, 280);
 });
 
