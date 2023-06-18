ae996b0f56ce7a33c782069a898a5b26acb4cf4a
spencer stecko <spencerstecko@spencers-MacBook-Pro.local>
2023-06-18 16:47:09 -0600

    add multiple ads for one ad

--- hgapp/ads/models.py
+++ hgapp/ads/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+import random
 
 
 class FakeAd(models.Model):
     picture = models.ImageField(null=True, blank=True)
     banner = models.ImageField()
+    banner_2 = models.ImageField(blank=True)
+    banner_3 = models.ImageField(blank=True)
     headline = models.CharField(max_length=3000)
     subject = models.CharField(max_length=3000)
     content = models.TextField(max_length=20000)
+
+    def get_random_banner_url(self):
+        banners = [self.banner, self.banner_2, self.banner_3]
+        banners = [x for x in banners if x.name]
+        random.shuffle(banners)
+        return banners[0].url
