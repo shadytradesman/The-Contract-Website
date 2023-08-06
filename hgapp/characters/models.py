f688249bbf7a922f5d12767f83702a50f503130e
spencer stecko <spencerstecko@spencers-MacBook-Pro.local>
2023-08-06 14:52:31 -0600

    fix improvement allocation for ported characters

--- hgapp/characters/models.py
+++ hgapp/characters/models.py
@@ -863,8 +863,7 @@ class Character(models.Model):
         return rewards_to_be_spent
 
     def improvement_ok(self):
-        ported_adjustment = PORTED_IMPROVEMENT_ADJUSTMENT[self.ported] + PORTED_GIFT_ADJUSTMENT[self.ported]
-        return self.number_of_victories() * 2 > (self.num_active_rewards() - ported_adjustment)
+        return (self.number_of_victories() + PORTED_GIFT_ADJUSTMENT[self.ported] )* 2 > (self.num_active_rewards())
 
     def get_powers_for_render(self):
         return self.power_full_set.all()
