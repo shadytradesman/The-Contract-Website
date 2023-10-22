94c651f3dea503456652eb7730d075e266c25339
spencer stecko <spencerstecko@spencers-MacBook-Pro.local>
2023-10-22 09:53:19 -0600

    bounced emails added to list

--- hgapp/emails/models.py
+++ hgapp/emails/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class BouncedEmail(models.Model):
+    email = models.CharField(max_length=900)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
--- hgapp/emails/signals.py
+++ hgapp/emails/signals.py
@@ -1,8 +1,11 @@
 from django.dispatch import receiver
 from games.models import Game_Invite, NotifyGameInvitee, GameChangeStartTime, GameEnded
 from django.conf import settings
-
+from django_ses.signals import bounce_received
 from emails.tasks import start_time_change_notifications, game_ended_notifications, game_invite_notification
+
+from .models import BouncedEmail
+
 
 import logging
 
@@ -13,13 +16,13 @@ from_email = settings.DEFAULT_FROM_EMAIL
 NOTIF_VAR_UPCOMING = "upcoming"
 NOTIF_VAR_FINISHED = "finished"
 
+
 @receiver(GameChangeStartTime)
 def notify_changed_start(**kwargs):
     game = kwargs["game"]
     request = kwargs["request"]
     uri_prefix = request.build_absolute_uri('/')
     start_time_change_notifications.delay(game.pk, uri_prefix)
-
 
 
 @receiver(GameEnded)
@@ -30,11 +33,16 @@ def notify_game_ended(**kwargs):
     game_ended_notifications.delay(game.pk, uri_prefix)
 
 
-
-
 @receiver(NotifyGameInvitee)
 def notify_game_invitee(sender, **kwargs):
     invite = kwargs["game_invite"]
     request = kwargs["request"]
     uri_prefix = request.build_absolute_uri('/')
     game_invite_notification.delay(invite.pk, uri_prefix)
+
+@receiver(bounce_received)
+def bounce_handler(sender, mail_obj, bounce_obj, raw_message, *args, **kwargs):
+    recipient_list = mail_obj['destination']
+    for recipient in recipient_list:
+        logger.info("Email bounced: {}".format(recipient))
+        BouncedEmail.create(email=recipient)
