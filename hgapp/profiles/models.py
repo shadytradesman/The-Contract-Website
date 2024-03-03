cec62f18efe1bf2a4b1777bfa70b5995ac2f10b5
spencer stecko <spencerstecko@spencers-MacBook-Pro.local>
2024-03-03 09:29:55 -0700

    allow others to edit guidebook

--- hgapp/profiles/models.py
+++ hgapp/profiles/models.py
@@ -93,6 +93,7 @@ class Profile(models.Model):
     ps2_user = models.BooleanField(default=False)
     early_access_user = models.BooleanField(default=False)
     is_site_moderator = models.BooleanField(default=False)
+    can_edit_guidebook = models.BooleanField(default=False)
     date_confirmed_agreements = models.DateTimeField(blank=True, null=True)
     player_prefix = models.CharField(choices=PLAYER_PREFIX,
                                      max_length=25,
