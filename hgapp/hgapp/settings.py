a7b2970d109b8920809e8af70ca779b5558fd08a
spencer stecko <spencerstecko@spencers-MacBook-Pro.local>
2024-03-30 09:19:37 -0600

    add poised art to how to play

--- hgapp/hgapp/settings.py
+++ hgapp/hgapp/settings.py
@@ -352,11 +352,11 @@ else:
 if DEBUG:
     pass
     # #Comment this CACHES block out to test caching during development.
-    # CACHES = {
-    #     'default': {
-    #         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-    #     }
-    # }
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        }
+    }
 else:
     CACHES = {
         'default': {
