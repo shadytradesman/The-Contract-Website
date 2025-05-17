3f270af6bb304d4d6d679f83db43dbfb8d655f56
spencer stecko <spencerstecko@spencers-MacBook-Pro.local>
2025-05-17 12:49:05 -0600

    fix memcached

--- hgapp/hgapp/settings.py
+++ hgapp/hgapp/settings.py
@@ -361,7 +361,7 @@ if DEBUG:
 else:
     CACHES = {
         'default': {
-            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
             'LOCATION': '127.0.0.1:11211',
         }
     }
