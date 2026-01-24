4e4fd88c68666173525ddcfb818893ad310b201e
spencer stecko <spencerstecko@spencers-MacBook-Pro.local>
2026-01-24 13:07:39 -0700

    fix blog posts more

--- hgapp/blog/forms.py
+++ hgapp/blog/forms.py
@@ -31,6 +31,12 @@ class PostFormMixin:
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        post = self.instance
+        latest_revision = post.latest() if post.pk else None
+        if latest_revision:
+            # set initial data from the latest revision
+            self.fields["teaser"].initial = latest_revision.teaser
+            self.fields["content"].initial = latest_revision.content
 
     def save_post(self, post):
         published = False
